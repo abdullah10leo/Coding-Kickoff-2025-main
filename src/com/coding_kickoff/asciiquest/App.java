e3076e277d56ba69b5ef7638ce7cf2799cd129b7
abdullah10leo <abdullahimran2002@gmail.com>
2025-09-18 18:17:21 -0500

    Update App.java

--- src/com/coding_kickoff/asciiquest/App.java
+++ src/com/coding_kickoff/asciiquest/App.java
@@ -1,7 +1,13 @@
 package com.coding_kickoff.asciiquest;
 
 class App {
-    public static void main(String[] args) {
-        System.out.println("Hello world!");
+    public static void main(String[] args) {}
+    
+
+    public static void hello() {
+        System.out.println("Hello my friend");
+        
     }
 }
+
+
